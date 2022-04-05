f0d9d1fd87aeb5e829577f74433aba60ac3cec2a
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-04-05 11:14:40 +0400

    asdnfasjdn

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    enum Genre
+    {
+       genre,
+    };
+    internal class Book
+    {
+       
+        private int _no;
+        public int  No { get; set; }  
+        public string Name { get; set; }    
+        public string AuthorName { get; set; }  
+        public int Price { get; set; }  
+        public Book()
+        {
+            _no++;
+            No = _no;
+        }
+       
+    }
+}
---
+++ ConsoleApp1/Library.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Library
+    {
+      public  List<Book> books;
+      public List<Book> FilterByPrice(int min,int max)
+        {
+            var filters=books.FindAll(x=>x.Price>=min&&x.Price <=max);
+            return filters; 
+        }
+        List<Genre> genres;
+      public List<Genre> FilterByGenre(Genre name)
+        {
+            var filters1 = genres.FindAll(x => x.genre == x.name);
+              return filters1;
+        }
+        public Book FİndBookByNo(int? _no)
+        {
+            if (_no == null)
+            {
+                return null; 
+            }
+            return books.Find(x=>x.No==_no);
+
+        }
+    }
+}
